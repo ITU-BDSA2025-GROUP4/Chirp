c6282f102ca20a11c2670d69db1e85c0f4fd5e7b
aulh <aulh@itu.dk>
2025-08-29 09:46:05 +0200

    feat: read chirp file and display it
    Co-authors: Adam <adsw@itu.dk>, Morgan <moto@itu.dk>

--- Program.cs
+++ Program.cs
@@ -14,3 +14,21 @@ if (command == "chirp")
     File.AppendAllText("./chirp_cli_db.csv", name + ",\"" + message + "\"," + timestamp + Environment.NewLine);
     Console.WriteLine(name + " @ " + timestamp + ": " + message);
 }
+else if (command == "read")
+{
+    var lines = File.ReadLines("/chirp_cli_db.csv");
+    foreach (var currLine in lines.Skip(1))
+    {
+        var parts = currLine.Split(",");
+        foreach (var part in parts)
+        {
+            Console.WriteLine(part);
+        }
+        //        string author = parts[0];
+        //        string message = parts[1];
+        //        DateTimeOffset timestamp = DateTimeOffset.FromUnixTimeSeconds(int.Parse(parts[2]));
+        //
+        //        Console.WriteLine(author + " @ " + timestamp + ": " + message);
+    }
+
+}
