3d7d205240cdfbbd7bd2d2465f39aec48e07328e
aulh <aulh@itu.dk>
2025-08-29 09:42:42 +0200

    Added chirp functionality
    Co-authors: Adam <adsw@itu.dk>, Morgan <moto@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string command = args[0];
+
+if (command == "chirp")
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Please specify a message to chirp!");
+        return;
+    }
+    string name = Environment.UserName;
+    long timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+
+    string message = args[1];
+    File.AppendAllText("./chirp_cli_db.csv", name + ",\"" + message + "\"," + timestamp + Environment.NewLine);
+    Console.WriteLine(name + " @ " + timestamp + ": " + message);
+}
