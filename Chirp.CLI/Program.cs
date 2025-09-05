3713e3a3be1b824dd6f229471f7549170f6180d7
Mojjedrengen <moto@itu.dk>
2025-09-05 11:24:13 +0200

    now we have docopt working

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using Utils;
+using System.Collections.Generic;
+using DocoptNet;
 
 // Todo: Fix this, for Now I just Assumed this was the format, for the UI class :)
 public record Cheep(string Author, string Message, long Timestamp);
@@ -17,19 +19,20 @@ public static class UserInterface
     }
 }
 
-
-class ChirpMain
+static class ChirpMain
 {
     static void chirpExit(int statusCode) {
         Logger.get.Dispose();
         System.Environment.Exit(statusCode);
     }
 
+    const string chirpDbPath = "Chirp.CLI/chirp_cli_db.csv";
     static void read()
     {
         var cheeps = new List<Cheep>();
         
-        var lines = File.ReadLines("<path-to-db-file>");
+        //var lines = File.ReadLines("<path-to-db-file>");
+        var lines = File.ReadLines(chirpDbPath);
         foreach (var currLine in lines.Skip(1))
         {
             var parts = currLine.Split(",", 3);
@@ -57,10 +60,11 @@ class ChirpMain
         string name = Environment.UserName;
         long timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
 
-        File.AppendAllText("<path-to-db-file>", name + "," + timestamp +  ",\"" + message + "\"" + Environment.NewLine);
+        //File.AppendAllText("<path-to-db-file>", name + "," + timestamp +  ",\"" + message + "\"" + Environment.NewLine);
+        File.AppendAllText(chirpDbPath, name + "," + timestamp +  ",\"" + message + "\"" + Environment.NewLine);
     }
 
-    static void help()
+    static void helpfunc()
     {
         Console.WriteLine("Commands:\nchirp [MESSAGE] | Chirps a message\nread | Displays all chirps\n? | Displays this menu\nexit | Exits Chirp.CLI\n");
     }
@@ -83,7 +87,7 @@ class ChirpMain
                 chirp(args[1]);
                 break;
             case "?":
-                help();
+                helpfunc();
                 break;
             case "exit":
                 chirpExit(0);
@@ -98,7 +102,7 @@ class ChirpMain
     static void interactive()
     {
         Console.WriteLine("Running Chirp.CLI in interactive mode, type ? to re-display help");
-        help();
+        helpfunc();
 
         while (true)
         {
@@ -114,10 +118,62 @@ class ChirpMain
         }
     }
 
-    static void Main(string[] args)
+
+     const string help = @"Chirp.CLI.
+
+    Usage:
+      Chirp.CLI interactive
+      Chirp.CLI read
+      Chirp.CLI chirp <text>
+      Chirp.CLI (-h | --help)
+      Chirp.CLI --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+
+    ";
+
+    // 8 lines from DocoptNet docs
+    static int ShowHelp(string help) { Console.WriteLine(help); return 0; }
+    static int ShowVersion(string version) { Console.WriteLine(version); return 0; }
+    static int OnError(string usage) { Console.WriteLine(usage); return 1; }
+    static int Run(IDictionary<string, ArgValue> arguments)
+    {
+        bool chirpbool = false;
+        foreach (var (key, value) in arguments){
+            if (key == "interactive" && ((bool)value))
+                interactive();
+            if (key == "read" && ((bool)value)) 
+                read();
+            if (key == "chirp" && ((bool)value))
+                chirpbool = true;
+            if (key == "<text>" && chirpbool)
+                chirp(((string)value));
+            //Console.WriteLine("{0} = {1}", key, value);
+        }
+        return 0;
+    }
+
+    static void start(IDictionary<string, ArgValue> result) {
+        /*
+        if (result.Item[interactive] == true) {
+            interactive();
+        }
+        if (result.Item[read] == true) {
+            read();
+        }
+        if (result.Item[chirp] == true) {
+            chirp(result.Item[text]);
+        }*/ 
+        Console.WriteLine("start");
+    }
+
+    static int Main(string[] args)
     {
         // Uncomment the line below in order to disable all logging
 //        Logger.get.Disable();
+        /*
         if (args.Length == 0)
         {
             interactive();
@@ -126,8 +182,28 @@ class ChirpMain
         {
             batch(args);
         }
+        */
+        //
+        /*
+        return Docopt.CreateParser(help)
+                     .WithVersion("Naval Fate 2.0")
+                     .Parse(args)
+                     .Match(Run,
+                            result => ShowHelp(result.Help),
+                            result => ShowVersion(result.Version),
+                            result => OnError(result.Usage));
+        
+        */
+        return Docopt.CreateParser(help)
+                    .WithVersion("Chirp.CLI 0.2")
+                    .Parse(args)
+                    .Match(Run,
+                            result => ShowHelp(result.Help),
+                            result => ShowVersion(result.Version),
+                            result => OnError(result.Usage)
+                            );
 
-        chirpExit(0);
+//        chirpExit(0);
     }
 
 }
