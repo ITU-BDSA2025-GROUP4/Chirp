12f86bf875512e77ea63524ea4a61e4a395833a2
RockRottenSalad <adsw@itu.dk>
2025-09-12 13:02:32 +0200

    Add more debugging info to version meta script

--- scripts/generate_version.sh
+++ scripts/generate_version.sh
@@ -2,12 +2,12 @@
 
 FILEPATH="src/Chirp.CLI/Version.cs"
 NAMESPACE="MetaData"
-VERSION=${1}
+VERSION_RAW=${1}
 
-VERSION=$(echo -n "${VERSION}" | grep -Eo "^[^-]+")
+VERSION=$(echo -n "${VERSION_RAW}" | grep -Eo "^[^-]+")
 
 if [ -z $VERSION ]; then
-    echo "No version number given";
+    echo "No version number given: Raw version: ${VERSION_RAW}, trimmed version: ${VERSION}";
     exit 1;
 fi
 
