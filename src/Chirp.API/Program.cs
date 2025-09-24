032f80819d9b59cbde97eb226d4da6bd6b51527d
RockRottenSalad <adsw@itu.dk>
2025-09-24 15:36:59 +0200

    Refactored Chirp.API to use Chirp.APICore for handling requests

--- src/Chirp.API/Program.cs
+++ src/Chirp.API/Program.cs
@@ -1,67 +1,35 @@
-using Query;
+using Chirp.APICore;
 using Chirp.Types;
 using SimpleDB;
-
-using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-CsvDatabase<Cheep> db = new(Path.Combine(AppContext.BaseDirectory, "Resources", "Data", "chirp_cli_db.csv"));
+IDatabaseRepository<Cheep> db = 
+    new CsvDatabase<Cheep>(
+            Path.Combine(AppContext.BaseDirectory, "Resources", "Data", "chirp_cli_db.csv")
+);
+
+APICore core = new APICore(db);
+
+Dictionary<string, string> QueryToDict(IQueryCollection collection) 
+{
+    return collection
+        .Where(x => x.Value.ToString() != "")
+        .ToDictionary(x => x.Key, x => x.Value.ToString());
+}
 
 app.MapGet(
     "/cheeps",
-    (HttpRequest request, HttpResponse response) =>
-    {
-        var predicates = new List<Func<Cheep, bool>>();
-        var allQueryParamters = Enum.GetValues(typeof(QueryParamter)).Cast<QueryParamter>();
-
-        foreach(Query.QueryParamter x
-                in 
-                allQueryParamters) 
-        {
-            var queryKey = x.ToString();
-            var queryValue = request.Query[queryKey].ToString();
-
-            // If value was provided for queryKey, then add that rpedicate
-            if(queryValue != "" && x.KeyMatchesValue(queryValue)) {
-                predicates.Add( x.ToPredicate(queryValue) );
-            }
-        }
-
-        if(predicates.Count() > 0) {
-            // This applies all predicates on each cheep
-            return db.Query((Cheep x) => predicates.Aggregate(true,
-                        (acc, f) => acc && f(x)
-                    ));
-        }else {
-            return db.ReadAll();
-        }
-});
+    (HttpRequest request, HttpResponse response) => 
+        core.Cheeps( QueryToDict(request.Query) )
+    );
 
 app.MapGet(
     "/cheep",
     (HttpRequest request, HttpRequest response) => 
-    {
+        core.Cheep( QueryToDict(request.Query) )
 
-        var author = request.Query["author"].ToString();
-        var message = request.Query["message"].ToString();
-        var timestampStr = request.Query["timestamp"].ToString();
-
-        if(author == "") { return "Missing author"; } 
-        if(message == "") { return "Missing message"; } 
-        if(timestampStr == "") { return "Missing timestamp"; } 
-
-        long timestamp = 0;
-        if(!long.TryParse(timestampStr, out timestamp)) {
-            return "Timestamp must be in unix time format";
-        }
-
-        db.Store(new Cheep(author, message, timestamp));
-
-        return "Cheep'ed";
-
-    }
 );
 
 app.MapGet("/", () => "Use /cheeps");
@@ -69,4 +37,4 @@ app.MapGet("/", () => "Use /cheeps");
 app.Run();
 
 // Remember to save changes to DB before exiting
-db.Write();
+//db.Write();
---
+++ src/Chirp.APICore/APICore.cs
@@ -0,0 +1,149 @@
+﻿namespace Chirp.APICore;
+
+using Chirp.Types;
+using Utils;
+using SimpleDB;
+
+public class APICore {
+
+    private IDatabaseRepository<Cheep> db;
+//        = new(Path.Combine(AppContext.BaseDirectory, "Resources", "Data", "chirp_cli_db.csv"));
+
+    public APICore(IDatabaseRepository<Cheep> database) {
+        db = database;
+    }
+
+    public string Cheep(Dictionary<string, string> queryParameters) {
+
+        if(!queryParameters.ContainsKey("author")) {
+            return "Missing author";
+        }else if(!queryParameters.ContainsKey("message")) {
+            return "Missing message";
+        }else if(!queryParameters.ContainsKey("timestamp")) {
+            return "Missing timestamp";
+        }
+
+        var author = queryParameters["author"];;
+        var message = queryParameters["message"];;
+        var timestampStr = queryParameters["timestamp"];;
+
+        long timestamp = 0;
+        if(!long.TryParse(timestampStr, out timestamp)) {
+            return "Timestamp must be in unix time format";
+        }
+
+        db.Store(new Cheep(author, message, timestamp));
+
+        return "Cheep'ed: " + message + " by " + author + " " + timestamp;
+
+    }
+    public IEnumerable<Cheep> Cheeps(Dictionary<string, string> actualQueryParameters)
+    {
+        var predicates = new List<Func<Cheep, bool>>();
+        var allQueryParameters = Enum.GetValues(typeof(QueryParameter)).Cast<QueryParameter>();
+
+        foreach(APICore.QueryParameter x
+                in 
+                allQueryParameters) 
+        {
+            var queryKey = x.ToString();
+            var queryValue = actualQueryParameters.GetValueOrDefault(queryKey, "");
+
+            // If value was provided for queryKey, then add that rpedicate
+            if(queryValue != "" && Query.KeyMatchesValue(x, queryValue)) {
+                predicates.Add( Query.ToPredicate(x, queryValue) );
+            }
+        }
+
+        if(predicates.Count() > 0) {
+            Console.WriteLine("Pred, DB size: " + db.ReadAll().Count());
+            // This applies all predicates on each cheep
+            return db.Query((Cheep x) => predicates.Aggregate(true,
+                        (acc, f) => acc && f(x)
+                        ));
+        }else {
+            Console.WriteLine("All, DB size: " + db.ReadAll().Count());
+            return db.ReadAll();
+        }
+    }
+
+    public enum QueryParameter
+    {
+        byUsers        =  1 << 0,
+        notByUsers     =  1 << 1,
+        beforeTime     =  1 << 2,
+        afterTime      =  1 << 3,
+        cheepContains  =  1 << 4
+    }
+
+    public static class Query {
+
+        public static bool KeyMatchesValue(QueryParameter x, string value) {
+            switch(x) 
+            {
+                case QueryParameter.beforeTime:
+                    return StringUtils.IsInteger(value);
+                case QueryParameter.afterTime:
+                    return StringUtils.IsInteger(value);
+                default:
+                    return true;
+            }
+
+        }
+
+        // value MUST be sanitized before this is called
+        public static Func<Cheep, bool> ToPredicate(QueryParameter x, string value) 
+        {
+            switch(x) 
+            {
+                case QueryParameter.byUsers:
+                    return (Cheep cheep) => {
+                        if (value.Contains(',')) {
+                            return value.Split(',').Select(x => x.ToLower()).Contains(cheep.Author.ToLower());
+                        }
+                        return cheep.Author.ToLower() == value.ToLower();
+                    };
+                case QueryParameter.notByUsers:
+                    return (Cheep cheep) => {
+                        if (value.Contains(',')) {
+                            return !value.Split(',').Select(x => x.ToLower()).Contains(cheep.Author.ToLower());
+                        }
+
+                        return cheep.Author.ToLower() != value.ToLower();
+                    };
+                case QueryParameter.beforeTime:
+                    return (Cheep cheep) => cheep.Timestamp < long.Parse(value);
+                case QueryParameter.afterTime:
+                    return (Cheep cheep) => long.Parse(value) < cheep.Timestamp;
+                case QueryParameter.cheepContains:
+                    return (Cheep cheep) => cheep.Message.ToLower().Contains(value.ToLower());
+            }
+
+            // This should never happen since all cases are covered in the switch statement
+            return (Cheep cheep) => false;
+        }
+
+
+        public static String ToString(QueryParameter x) 
+        {
+            switch (x)
+            {
+                case QueryParameter.byUsers:
+                    return "byUsers";
+                case QueryParameter.notByUsers:
+                    return "notByUsers";
+                case QueryParameter.beforeTime:
+                    return "beforeTime";
+                case QueryParameter.afterTime:
+                    return "afterTime";
+                case QueryParameter.cheepContains:
+                    return "cheepContains";
+            }
+
+            // This shoulder never happen
+            return "";
+        }
+    }
+
+
+}
