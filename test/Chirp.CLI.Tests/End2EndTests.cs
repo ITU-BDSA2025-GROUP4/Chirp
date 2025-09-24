de1fe7ccaba1bdc43019387cbe30320f3053cf23
aulh <aulh@itu.dk>
2025-09-24 15:20:30 +0200

    Further work on test

--- test/Chirp.CLI.Tests/End2EndTests.cs
+++ test/Chirp.CLI.Tests/End2EndTests.cs
@@ -4,26 +4,49 @@ using Xunit;
 
 using Chirp.Cli;
 
-public class End2EndTsests
+public class End2EndTests
 {
 
     [Fact]
     public void TestReadCheeps()
     {
         var args = new string[] { "chirp", "read" };
+        ConsoleListener.Listen();
 
+        //        var result = ChirpMain.Main(args);
+        //        Assert.Equal(0, result);
+        //
+        Console.WriteLine("dk");
+        Console.WriteLine("idk");
+        var output = ConsoleListener.Export();
+
+
+        Console.WriteLine(output);
+
+        //nu assert at det passer med noget forventet :)
     }
 
-    [Fact]
-    public void TestReadTenCheeps()
-    {
-        var args = new string[] { "chirp", "read", "10" };
-    }
-
-    [Fact]
-    public void TestCheep()
-    {
-        var args = new string[] { "chirp", "Hello!!!" };
-        ChirpMain.Main(args);
-    }
+    //    [Fact]
+    //    public void TestReadTenCheeps()
+    //    {
+    //        var args = new string[] { "chirp", "read" };
+    //        ConsoleListener.Listen();
+    //
+    //        var result = ChirpMain.Main(args);
+    //        Assert.Equal(0, result);
+    //
+    //        var output = ConsoleListener.Export();
+    //
+    //        Console.WriteLine(output);
+    //        //nu assert at det passer med noget forventet :)
+    //    }
+    //
+    //    [Fact]
+    //    public void TestCheep()
+    //    {
+    //        //var args = new string[] { "chirp", "Hello!!!" };
+    //        //Assert.Equal(0, ChirpMain.Main(args));
+    //
+    //        // Now read and see if the chirp we added is there
+    //    }
 }
