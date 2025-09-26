66d83213511310ed1953f5ae665bacabc341ecca
aulh <aulh@itu.dk>
2025-09-26 16:02:35 +0200

    Readded UnixTimeStamp test from Chirp.CLI

--- test/Chirp.Razor.Tests/UnitTests.cs
+++ test/Chirp.Razor.Tests/UnitTests.cs
@@ -2,9 +2,13 @@
 
 public class UnitTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+    [Theory]
+    [InlineData(0, "01/01/1970 00:00:00")]
+    [InlineData(-1758894113, "07/04/1914 10:18:07")]
+    [InlineData(1690895308, "01/08/2023 13:08:28")]
+    public void TestUnixTimeStampToDateTimeString(long timestamp, string expected)
+    {
+        Assert.Equal(expected, CheepServiceUtils.UnixTimeStampToDateTimeString(timestamp));
     }
 }
