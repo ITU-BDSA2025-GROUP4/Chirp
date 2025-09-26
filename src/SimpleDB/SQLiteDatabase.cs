92168d695757a6083575ba7dcd2fd0d9511a32d6
RockRottenSalad <adam19sw@gmail.com>
2025-09-26 19:54:50 +0200

    SQLDatabase somewhat functional read method

--- src/SimpleDB/SQLiteDatabase.cs
+++ src/SimpleDB/SQLiteDatabase.cs
@@ -1,49 +1,80 @@
 namespace SimpleDB;
 
+// TODO: Check that given table exists in file
+
+using Utils;
 using System;
 using System.Data;
 using System.Data.SQLite;
 
+public record Test(string author, string message, long timestamp) : ISQLType<Test>
+{
+
+    public static Test Extract(SQLiteDataReader reader)
+    {
+        int id = reader.GetInt32(0);
+        string author = reader.GetString(1);
+        string message = reader.GetString(2);
+        long timestamp = reader.GetInt64(3);
+
+        return new Test(author, message, timestamp);
+    }
+
+    public static string ExpectedTable()
+    {
+        return "Cheep";
+    }
+
+}
 
 public sealed class SQLiteDatabase<T> : IDatabaseRepository<T> where T : ISQLType<T>
 {
 
-    private SQLiteConnection conn;
+    private SQLiteConnection _conn;
+    private string _table;
 
-    public SQLiteDatabase(string filepath)
+    public SQLiteDatabase(string filepath, string table)
     {
         if(!Path.Exists(filepath)) 
             throw new FileNotFoundException("Could not find: " + filepath);
         else
             filepath = Path.GetFullPath(filepath);
 
-        conn = new SQLiteConnection("Data Source=" + filepath);
+        _conn = new SQLiteConnection("Data Source=" + filepath);
+        _table = table;
 
-        conn.Open();
+        _conn.Open();
+
+        if(!DoesTableExist(table)) {
+            throw new Exception("Welp");
+        }
+
     }
 
-    ~SQLiteDatabase() {
-        conn.Close();
+    ~SQLiteDatabase()
+    {
+        _conn.Close();
     }
 
     public IEnumerable<T> Read(int limit)
     {
-        SQLiteCommand cmd = SQLqueries.SelectLatestCheeps("Cheep", limit, conn);
+        SQLiteCommand cmd = SQLqueries.SelectLatestCheeps(_conn, limit);
 
         List<T> results = new List<T>();
 
+        DataTable dt = new DataTable();
+        dt.Load( cmd.ExecuteReader() );
+
         using (SQLiteDataReader reader = cmd.ExecuteReader()) 
         {
-            if(!reader.HasRows) {
-                // Shit
-            }
-
+            if(reader.HasRows)
             while(reader.Read()) {
-                results.Add(ISQLType<T>.Extract(reader));
+                
+                results.Add(T.Extract(reader));
             }
         }
 
-        throw new NotImplementedException();
+        return results;
     }
 
     public IEnumerable<T> ReadAll()
@@ -51,21 +82,54 @@ public sealed class SQLiteDatabase<T> : IDatabaseRepository<T> where T : ISQLTyp
         return Read(Size());
     }
 
-    public IEnumerable<T> Query(Func<T, bool> condition) 
+    public IEnumerable<T> Query(Func<T, bool> condition)
     {
         return ReadAll().Where(condition);
     }
 
-    public void Store(T record) {
-
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
     }
 
-    public void Write() {
+    public void Write()
+    {
+        throw new NotImplementedException();
     }
 
     public int Size()
     {
+        var cmd = SQLqueries.SelectCountTable(_conn, "Cheep");
+
+        using (SQLiteDataReader reader = cmd.ExecuteReader()) 
+        {
+            if(!reader.HasRows) {
+                return 0;
+            }
+
+            if(reader.Read())
+            {
+                return reader.GetInt32(0);
+            }
+        }
+
         return 0;
     }
 
+    private bool DoesTableExist(string table) 
+    {
+        var cmd = SQLqueries.SelectTableNames(_conn);
+
+        using (SQLiteDataReader reader = cmd.ExecuteReader()) 
+        {
+            if(!reader.HasRows) {
+                return false;
+            }
+            while(reader.Read()) {
+                string tableInDatabase = reader.GetString(0);
+                if(tableInDatabase == table) return true;
+            }
+        }
+        return false;
+    }
 }
--- src/SimpleDB/SQLqueries.cs
+++ src/SimpleDB/SQLqueries.cs
@@ -4,23 +4,41 @@ using System.Data.SQLite;
 
 public static class SQLqueries {
 
-    private static string extractTableNamesj 
-        = "SELECT NAME from sqlite_master";
+    private static string selectTableNames
+        = "SELECT NAME FROM sqlite_master;";
 
     // {0} = string | Table name
-    // {1} = int | Limit
-    private static string SelectLatestCheepsCommand 
-        = "SELECT * FROM {0} ORDER BY timestamp DESC LIMIT {1};";
+    private static string countTableEntries
+        = "SELECT count(*) FROM {0};";
 
-    
-    public static SQLiteCommand SelectLatestCheeps(string table, int limit, SQLiteConnection conn) {
-        return new SQLiteCommand(String.Format(SelectLatestCheepsCommand, table, limit), conn);
+    // {0} = string | Table name
+    // {1} = string | Order by
+    // {2} = string | ASC or DESC
+    // {3} = int | Limit
+    private static string selectOrderByDescQuery 
+        = "SELECT * FROM {0} ORDER BY {1} {2} LIMIT {3};";
+
+    public static SQLiteCommand SelectCountTable(SQLiteConnection conn, string table)
+    {
+        return new SQLiteCommand(String.Format(countTableEntries, table), conn);
+    }
+
+
+    public static SQLiteCommand SelectTableNames(SQLiteConnection conn)
+    {
+        return new SQLiteCommand(selectTableNames, conn);
+    }
+
+    public static SQLiteCommand SelectLatestCheeps(SQLiteConnection conn, int limit) 
+    {
+        return new SQLiteCommand(String.Format(selectOrderByDescQuery, "Cheep", "timestamp", "DESC", limit), conn);
     }
 
 }
 
 public interface ISQLType<T>
 {
-    public static T Extract(SQLiteDataReader reader) 
-        => throw new Exception("ISQlType<T>.Extract() Must be overriden");
+    public static abstract T Extract(SQLiteDataReader reader);
+
+    public static abstract string ExpectedTable();
 }
