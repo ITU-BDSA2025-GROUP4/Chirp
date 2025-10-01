fe73aa6fbc73738606bdd58c619f189feea51812
RockRottenSalad <adsw@itu.dk>
2025-10-01 15:07:34 +0200

    Add EnsureCreated() call inside DbContext, to ensure the tables are present

--- src/Chirp.Razor/Data/ChirpDBContext.cs
+++ src/Chirp.Razor/Data/ChirpDBContext.cs
@@ -5,7 +5,10 @@ namespace Chirp.Razor.Data;
 
 public class ChirpDbContext : DbContext
 {
-    public ChirpDbContext(DbContextOptions<ChirpDbContext> options) : base(options) { }
+    public ChirpDbContext(DbContextOptions<ChirpDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
     public DbSet<Cheep> Cheeps => Set<Cheep>();
     public DbSet<Author> Authors => Set<Author>();
