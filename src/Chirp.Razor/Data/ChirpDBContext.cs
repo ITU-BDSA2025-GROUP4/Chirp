34191a92e13d353d72cd868ae57210a2cbb7c3a6
aulh <aulh@itu.dk>
2025-10-01 15:15:34 +0200

    Removed EnsureCreated() to fix compatability issues with Migrate

--- src/Chirp.Razor/Data/ChirpDBContext.cs
+++ src/Chirp.Razor/Data/ChirpDBContext.cs
@@ -5,10 +5,7 @@ namespace Chirp.Razor.Data;
 
 public class ChirpDbContext : DbContext
 {
-    public ChirpDbContext(DbContextOptions<ChirpDbContext> options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+    public ChirpDbContext(DbContextOptions<ChirpDbContext> options) : base(options) { }
 
     public DbSet<Cheep> Cheeps => Set<Cheep>();
     public DbSet<Author> Authors => Set<Author>();
