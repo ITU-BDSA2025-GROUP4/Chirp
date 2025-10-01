f36a37409017c814e256bfc4b21aa4475628ba4c
aulh <aulh@itu.dk>
2025-10-01 14:20:25 +0200

    Removed call to echo

--- src/Chirp.Razor/initdb.sh
+++ src/Chirp.Razor/initdb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo export PATH="$PATH:$HOME/.dotnet/tools/"
+export PATH="$PATH:$HOME/.dotnet/tools/"
 
 # Should probably use descriptive names for the migrations, cba tho
 # Also maybe we can do this in the c# file rather that this bash script
