8d437b3747e986cd60b48dbff962fb7efa7bcac5
aulh <aulh@itu.dk>
2025-10-01 04:36:43 +0200

    Adjusted pages to be async

--- src/Chirp.Razor/Pages/Public.cshtml.cs
+++ src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.Razor.Models;
 
 namespace Chirp.Razor.Pages;
 
@@ -8,17 +9,16 @@ public class PublicModel : PageModel
     private static readonly int _pageSize = 32;
 
     private readonly ICheepService _service;
-    public List<CheepViewModel> Cheeps { get; set; }
+    public IEnumerable<CheepViewModel> Cheeps { get; set; }
 
     public PublicModel(ICheepService service)
     {
         _service = service;
     }
 
-    public ActionResult OnGet([FromQuery] int page = 1)
+    public async Task OnGetAsync([FromQuery] int page = 1)
     {
         page = page > 1 ? page : 1;
-        Cheeps = _service.GetCheeps().Skip((page - 1) * _pageSize).Take(_pageSize).ToList();
-        return Page();
+        Cheeps = await _service.GetCheeps(page, _pageSize);
     }
 }
