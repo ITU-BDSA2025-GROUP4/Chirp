670a4afc80b14f149067d037e4f66507c971f435
augustlh <aulh@itu.dk>
2025-10-10 17:10:30 +0200

    Moved TimeStampUtils test to Utils folder and added missing unittests

---
+++ test/Chirp.Utils.Tests/TimeStampUtilsTests.cs
@@ -0,0 +1,39 @@
+﻿using Chirp.Core.Utils;
+
+namespace Chirp.Core.Tests.Unit;
+
+public class UnitTests
+{
+
+    [Theory]
+    [InlineData(0, "01/01/1970 00:00:00")]
+    [InlineData(-1758894113, "07/04/1914 10:18:07")]
+    [InlineData(1690895308, "01/08/2023 13:08:28")]
+    public void TestUnixTimeStampToDateTimeString(long timestamp, string expected)
+    {
+        Assert.Equal(expected, TimestampUtils.UnixTimeStampToDateTimeString(timestamp));
+    }
+
+
+    [Theory]
+    [InlineData("01/01/1970 00:00:00")]
+    [InlineData("07/04/1914 10:18:07")]
+    [InlineData("01/08/2023 13:08:28")]
+    public void TestDateTimeTimeStampToDateTimeString(string dateTimeString)
+    {
+        DateTime dt = DateTime.ParseExact(dateTimeString, "dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+        string result = TimestampUtils.DateTimeTimeStampToDateTimeString(dt);
+        Assert.Equal(dateTimeString, result);
+    }
+
+    [Theory]
+    [InlineData("01/01/1970 00:00:00")]
+    [InlineData("07/04/1914 10:18:07")]
+    [InlineData("01/08/2023 13:08:28")]
+    public void TestDateTimeStringToDateTimeTimeStamp(string dateTimeString)
+    {
+        DateTime dt = TimestampUtils.DateTimeStringToDateTimeTimeStamp(dateTimeString);
+        string result = dt.ToString("dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.InvariantCulture);
+        Assert.Equal(dateTimeString, result);
+    }
+}
