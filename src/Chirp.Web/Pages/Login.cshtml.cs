a971688ffc7f34f75d70a7ffc829bfa018a63fab
mojjedrengen <morgan@famtorp.net>
2025-10-28 13:40:22 +0100

    add the view models for the login and registration

---
+++ src/Chirp.Web/Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Razor.Pages;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Email is Required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Remember Me")]
+    public bool RememberMe { get; set; }
+}
---
+++ src/Chirp.Web/Pages/Register.cshtml.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Razor.Pages;
+
+public class RegisterViewModel
+{
+    [Required]
+    [Display(Name = "UserName")]
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+    public string UserNamer { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = null!;
+
+    // TODO: add use later
+    [DataType(DataType.Date)]
+    [Display(Name = "Date of Birth")]
+    public DateTime? DateOfBirth { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Pawwrod and confirmation password do not match")]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPasswrod { get; set; } = null!;
+}
