22a70369f2ee934ecb70ab22504994274c6a4fbb
augustlh <aulh@itu.dk>
2025-11-16 22:35:08 +0100

    feat: fixed weird spelling mistake

--- src/Chirp.Core/Interfaces/ICheepRepository.cs
+++ src/Chirp.Core/Interfaces/ICheepRepository.cs
@@ -9,9 +9,9 @@ namespace Chirp.Core.Interfaces;
 public interface ICheepRepository : IRepository<CheepDTO>
 {
     public Task<List<CheepDTO>> ReadAsync(int pageNumber, int pageSize);
-    public Task<List<CheepDTO>> QueryAsync(Expression<Func<Cheep, bool>> condition, int pageNumber, int 
-        pageSIze);
+    public Task<List<CheepDTO>> QueryAsync(Expression<Func<Cheep, bool>> condition, int pageNumber, int
+        pageSize);
     public Task<AppResult<CheepDTO>> CreateAsync(CreateCheepRequest dto);
     public Task<AppResult<CheepDTO>> UpdateAsync(UpdateCheepRequest dto);
     public Task<AppResult> DeleteAsync(DeleteCheepRequest dto);
-}   
+}
