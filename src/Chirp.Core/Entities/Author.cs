30bff38dfeebf797ee7222d9acb680f57fd9e0b7
augustlh <aulh@itu.dk>
2025-11-16 22:36:30 +0100

    feat: added proper default values and removed unnecessary Email overwrite

--- src/Chirp.Core/Entities/Author.cs
+++ src/Chirp.Core/Entities/Author.cs
@@ -5,17 +5,14 @@ namespace Chirp.Core.Entities;
 // Annotations are set inside Chirp.Infrastructure/Data/ChirpDBContext.cs
 public class Author : IdentityUser<int>
 {
-    public override string Email { get; set; } = default!;
-
     public string Name // For backwards combability. TODO: Remove later 
     {
         get { return UserName!; }
         set { UserName = value; }
     }
 
-    public List<Cheep> Cheeps { get; set; } = null!;
-
-    public List<Follow> Following { get; set; } = null!;
-    public List<Follow> Followers { get; set; } = null!;
+    public List<Cheep> Cheeps { get; set; } = [];
+    public List<Follow> Following { get; set; } = [];
+    public List<Follow> Followers { get; set; } = [];
 
 }
