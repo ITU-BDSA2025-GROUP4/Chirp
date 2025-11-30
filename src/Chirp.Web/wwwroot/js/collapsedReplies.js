926da91c77fbaa3e637a2db21aff9ab535d41466
RockRottenSalad <adam19sw@gmail.com>
2025-11-30 15:17:51 +0100

    Add button to reply input field

--- src/Chirp.Web/wwwroot/js/collapsedReplies.js
+++ src/Chirp.Web/wwwroot/js/collapsedReplies.js
@@ -1,15 +1,18 @@
 
 var buttons = document.getElementsByClassName("collapseButton");
 
+const hiddenRepliesText = "Show remaining replies";
+const shownRepliesText = "Hide replies";
+
 for (var button of buttons) {
     button.addEventListener("click", function() {
         let replies = this.previousElementSibling;
 
         if (replies.style.display === "block") {
-            button.innerHTML = "Show remaining replies"
+            button.innerHTML = hiddenRepliesText;
             replies.style.display = "none";
         } else {
-            button.innerHTML = "Hide replies"
+            button.innerHTML = shownRepliesText;
             replies.style.display = "block";
         }
     })
@@ -18,8 +21,19 @@ for (var button of buttons) {
 function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
+function reply_input_hide(button) {
+    let replyInputForm = button.nextElementSibling;
+    button.style.display = "block";
+    replyInputForm.style.display = "none";
+}
 
 function toggle_reply_input(button) {
+    // Hide other reply input fields first
+    let otherReplyButtons = document.getElementsByClassName("displayReplyInputButton");
+    for(var otherButton of otherReplyButtons) {
+        reply_input_hide(otherButton);
+    }
+
     let replyInputForm = button.nextElementSibling;
     button.style.display = "none";
     replyInputForm.style.display = "block";
@@ -27,6 +41,4 @@ function toggle_reply_input(button) {
     let replyInputField = replyInputForm.getElementsByClassName("ReplyInputField")[0];
 
     replyInputField.focus();
-
-    console.log(document.activeElement===replyInputField);
 }
