9528310f24540807a4d9b4db2de33a879a009aa9
RockRottenSalad <adam19sw@gmail.com>
2026-01-01 16:16:23 +0100

    More comments

--- src/Chirp.Core/Utils/StringUtils.cs
+++ src/Chirp.Core/Utils/StringUtils.cs
@@ -22,9 +22,17 @@ public class StringUtils
             return false;
         }
 
+        // Checks that the remaining characters in the string are digits
         return x.Select(x => char.IsDigit(x)).Aggregate((x, y) => x && y);
     }
 
+    // Returns filename from file path
+    // Given a file path such as: "/dir/foo/example.txt"
+    // This will return "example.txt"
+    //
+    // KNOWN BUG:
+    // Will not work correctly with windows root filepaths i.e. "c:example.txt" will return "c:example.txt"
+    // Although will handle other cases fine: "c:\dir\example.txt" will return "example.txt"
     public static string GetFileName(string filepath)
     {
         int begin = -1;
@@ -45,6 +53,9 @@ public class StringUtils
         return filepath.Substring(begin + 1);
     }
 
+    // Strips away filename from filepath
+    // I.e. "/dir/foo/example.txt" will reutrn "/dir/foo"
+    // File extension is not required. I.e. "/dir/foo" will return "/dir"
     public static string GetDirectoryPath(string filepath) 
     {
         int end = -1;
@@ -60,6 +71,7 @@ public class StringUtils
         return filepath.Substring(0, end + 1);
     }
 
+    // Generates unique file path inside given directory with a specific file format extension
     public static string UniqueFilePath(string dir, string format) 
     {
         Random rng = new();
@@ -86,6 +98,7 @@ public class StringUtils
         return sb.ToString();
     }
 
+    // Converts a DateTimeOffset to a string using date, month, year format instead of the default month, date, year
     public static string TimeToString(DateTimeOffset time) 
     {
         string format = "dd/MM/yy HH:mm:ss";
